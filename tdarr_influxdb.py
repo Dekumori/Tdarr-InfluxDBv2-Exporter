e9e1d2e33dadf9d1311125543108bad0e5426f80
Dekumori <johnackelley@gmail.com>
2023-06-11 20:11:04 -0500

    Divided into discrete functions w/ docstrings

--- tdarr_influxdb.py
+++ tdarr_influxdb.py
@@ -4,114 +4,245 @@ import pkg_resources
 import json
 import re
 import os
-
-required = {'requests', 'configparser', 'influxdb_client'}
-installed = {pkg.key for pkg in pkg_resources.working_set}
-missing = required - installed
-
-if missing:
-    python = sys.executable
-    subprocess.check_call([python, '-m', 'pip', 'install', *missing], stdout=subprocess.DEVNULL)
-
 import requests
 import configparser
 import influxdb_client
 from influxdb_client.client.write_api import SYNCHRONOUS
 
-config = configparser.RawConfigParser()
-config.read(os.path.join(sys.path[0], "tdarr_influxdb.conf"))
+def install_missing_packages():
+    """
+    Install any missing Python packages required by the script.
 
-url = config['Tdarr']['proto']+"://"+config['Tdarr']['hostname']+":"+config['Tdarr']['port']+"/api/v2/cruddb"
-payload = {"data": {
-        "collection": "StatisticsJSONDB",
-        "mode": "getAll",
-        "docID": "statistics"
-    }}
-headers = {"content-type": "application/json"}
+    This function checks for missing packages and installs them using pip.
 
-response = requests.post(url, json=payload, headers=headers)
-tdarr_api = json.loads(response.content)
+    Packages:
+        sys, subprocess, pkg_resources
 
-influx_bucket = config['Influx']['bucket']
-influx_org = config['Influx']['org']
-influx_token = config['Influx']['token']
-influx_url = config['Influx']['proto']+"://"+config['Influx']['hostname']+":"+config['Influx']['port']
+    Args:
+        None
 
-influx_client = influxdb_client.InfluxDBClient(
-   url = influx_url,
-   token = influx_token,
-   org = influx_org
-)
+    Returns:
+        None
+    """
+    required = {'requests', 'configparser', 'influxdb_client'}
+    installed = {pkg.key for pkg in pkg_resources.working_set}
+    missing = required - installed
 
-write_api = influx_client.write_api(write_options=SYNCHRONOUS)
+    if missing:
+        python = sys.executable
+        subprocess.check_call([python, '-m', 'pip', 'install', *missing], stdout=subprocess.DEVNULL)
 
-p = influxdb_client.Point("TESTING").tag("statistics","totalFileCount").field("value",10345)
-write_api.write(bucket=influx_bucket, org=influx_org, record=p)
+def read_config():
+    """
+    Read the configuration file and return the parsed configuration.
 
-table = re.compile('table\dCount')
-table_num = re.compile('\d')
-string_to_int = re.compile('[a-zA-Z\s]*')
-tables = ["Hold","Transcode_Queue","Transcode_Success","Transcode_Error","Health_Check_Queue","Health_Check_Healthy","Health_Check_Error"]
-libraries = ["FileCount","Transcodes","Space_Saved","Num_of_Health_Checks","Transcode_Status","Health_Check_Status","Count_by_Codec","Count_by_Container","Count_by_Quality"]
-field = ["Status","Status","Codec","Container","Quality"]
-field_status = ["Not_Required","Queued","Success","Hold","Error","Ignored"]
+    This function reads the config file and parses it's contents using configparser.
+    It then returns the parsed config for use by other functions.
 
-for i in tdarr_api[0]:
-    if i not in set(["pies", "streamStats","languages","_id"]):
-        if re.search(table,i):
-            match = re.search(table_num,i)
-            items = match.group()
-            if type(tdarr_api[0][i]) == int or type(tdarr_api[0][i]) == float:
-                p = influxdb_client.Point("tdarr").tag(tdarr_api[0]["_id"],tables[int(items)]).field("value", "{:f}".format(tdarr_api[0][i]))
-                write_api.write(bucket=influx_bucket, org=influx_org, record=p)
-        else:
-            if type(tdarr_api[0][i]) == bool:
-                p = influxdb_client.Point("tdarr").tag(tdarr_api[0]["_id"], i).field("value", "{:f}".format(tdarr_api[0][i]))
-                write_api.write(bucket=influx_bucket, org=influx_org, record=p)
-            elif type(tdarr_api[0][i]) == int or type(tdarr_api[0][i]) == float:
-                p = influxdb_client.Point("tdarr").tag(tdarr_api[0]["_id"], i).field("value", "{:f}".format(tdarr_api[0][i]))
-                write_api.write(bucket=influx_bucket, org=influx_org, record=p)
-            elif tdarr_api[0][i] == "":
-                p = influxdb_client.Point("tdarr").tag(tdarr_api[0]["_id"], i).field("value", "{:f}".format(0))
-                write_api.write(bucket=influx_bucket, org=influx_org, record=p)
-            elif i != "DBLoadStatus":
-                strint = re.sub(string_to_int,"",tdarr_api[0][i])
-                p = influxdb_client.Point("tdarr").tag(tdarr_api[0]["_id"], i).field("value", "{:f}".format(float(strint)))
-                write_api.write(bucket=influx_bucket, org=influx_org, record=p)
+    Packages:
+        os, configparser
+
+    Args:
+        None
+
+    Returns:
+        configparser.RawConfigParser: The parsed configuration object.
+    """
+    config = configparser.RawConfigParser()
+    config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "tdarr_influxdb.conf")
+    with open(config_path) as f:
+        config.read_file(f)
+    return config
+
+def scrape_tdarr_api(config):
+    """
+    Scrape the TDarr API and retrieve library statistics.
+
+    This function sends a POST request to the TDarr API from the config file and returns it as a library.
+
+    Packages:
+        requests, json
+
+    Args:
+        config (configparser.RawConfigParser): The parsed configuration object.
+
+    Returns:
+        dict: The statistics data returned by the TDarr API.
+    """
+    url = config['Tdarr']['proto']+"://"+config['Tdarr']['hostname']+":"+config['Tdarr']['port']+"/api/v2/cruddb"
+    payload = {"data": {
+            "collection": "StatisticsJSONDB",
+            "mode": "getAll",
+            "docID": "statistics"
+        }}
+    headers = {"content-type": "application/json"}
+
+    try:
+        response = requests.post(url, json=payload, headers=headers)
+    except requests.exceptions.RequestException as e:
+        print("Network error:", e)
+
+    return json.loads(response.content)
+
+def connect_influxdb(config):
+    """
+    Connect to the InfluxDB instance using the provided configuration.
+
+    This function establishes a connection to the InfluxDB instance using the credentials from the config file.
+    It then returns the information needed to write to InfluxDBv2 for the write function.
+
+    Packages:
+        sys, influxdb_client, influx_client.client.write_api > SYNCHRONOUS
+
+    Args:
+        config (configparser.RawConfigParser): The parsed configuration object.
+
+    Returns:
+        tuple: A tuple containing the InfluxDB write API object, the InfluxDB bucket, and the InfluxDB organization.
+    """
+    influx_bucket = config['Influx']['bucket']
+    influx_org = config['Influx']['org']
+    influx_token = config['Influx']['token']
+    influx_url = config['Influx']['proto']+"://"+config['Influx']['hostname']+":"+config['Influx']['port']
+
+    influx_client = influxdb_client.InfluxDBClient(
+    url = influx_url,
+    token = influx_token,
+    org = influx_org
+    )
+
+    try:
+        write_api = influx_client.write_api(write_options=SYNCHRONOUS)
+    except Exception as e:
+        print("An error occurred:", e)
+        sys.exit(1)
+    return write_api, influx_bucket, influx_org
+
+def parse_tdarr(tdarr_api):
+    """
+    Parse the TDarr API response and generate InfluxDB data points.
+
+    This function takes the TDarr API response, extracts relevant data, and formats it into InfluxDB data points.
+    It returns a list of InfluxDB data points to be written by the write function.
+
+    Packages:
+        re, influxdb_client
+
+    Args:
+        tdarr_api (dict): The TDarr API response containing the statistics data.
+
+    Returns:
+        list: A list of InfluxDB data points representing the parsed statistics.
+    """
+    table = re.compile('table\dCount')
+    table_num = re.compile('\d')
+    string_to_int = re.compile('[a-zA-Z\s]*')
+    tables = ["Hold","Transcode_Queue","Transcode_Success","Transcode_Error","Health_Check_Queue","Health_Check_Healthy","Health_Check_Error"]
+    libraries = ["FileCount","Transcodes","Space_Saved","Num_of_Health_Checks","Transcode_Status","Health_Check_Status","Count_by_Codec","Count_by_Container","Count_by_Quality"]
+    field = ["Status","Status","Codec","Container","Quality"]
+    field_status = ["Not_Required","Queued","Success","Hold","Error","Ignored"]
+
+    record = []
+
+    for item_key in tdarr_api[0]:
+        if item_key not in set(["pies", "streamStats","languages","_id"]):
+            if re.search(table,item_key):
+                match = re.search(table_num,item_key)
+                items = match.group()
+                if type(tdarr_api[0][item_key]) == int or type(tdarr_api[0][item_key]) == float:
+                    influx_point = influxdb_client.Point("tdarr").tag(tdarr_api[0]["_id"],tables[int(items)]).field("value", "{:f}".format(tdarr_api[0][item_key]))
             else:
-                if tdarr_api[0][i] == "Stable":
-                    status = 0
+                if type(tdarr_api[0][item_key]) == bool:
+                    influx_point = influxdb_client.Point("tdarr").tag(tdarr_api[0]["_id"], item_key).field("value", "{:f}".format(tdarr_api[0][item_key]))
+                elif type(tdarr_api[0][item_key]) == int or type(tdarr_api[0][item_key]) == float:
+                    influx_point = influxdb_client.Point("tdarr").tag(tdarr_api[0]["_id"], item_key).field("value", "{:f}".format(tdarr_api[0][item_key]))
+                    
+                elif tdarr_api[0][item_key] == "":
+                    influx_point = influxdb_client.Point("tdarr").tag(tdarr_api[0]["_id"], item_key).field("value", "{:f}".format(0))
+                elif item_key != "DBLoadStatus":
+                    strint = re.sub(string_to_int,"",tdarr_api[0][item_key])
+                    influx_point = influxdb_client.Point("tdarr").tag(tdarr_api[0]["_id"], item_key).field("value", "{:f}".format(float(strint)))
                 else:
-                    status = 9999
-                p = influxdb_client.Point("tdarr").tag(tdarr_api[0]["_id"], i).field("value", "{:f}".format(status))
-                write_api.write(bucket=influx_bucket, org=influx_org, record=p)
-    elif i == "pies":
-        j = 0
-        while j < len(tdarr_api[0][i]):
-            k = 0
-            while k <= 3:
-                p = influxdb_client.Point("tdarr").tag(tdarr_api[0]["_id"], libraries[k]).tag("library_name", tdarr_api[0][i][j][0]).tag("library_id", tdarr_api[0][i][j][1]).field("value", "{:f}".format(tdarr_api[0][i][j][k+2]))
-                write_api.write(bucket=influx_bucket, org=influx_org, record=p)
-                k = k + 1
-            while k < len(tdarr_api[0][i][j])-4:
-                l = 0
-                while l < len(tdarr_api[0][i][j][k+2]):
-                    no_space = re.sub("\s","_",tdarr_api[0][i][j][k+2][l]["name"])
-                    if k < 1:
-                        p = influxdb_client.Point("tdarr").tag(tdarr_api[0]["_id"], libraries[k]).tag("library_name", tdarr_api[0][i][j][0]).tag("library_id", tdarr_api[0][i][j][1]).tag(field[k-4], field_status[l]).field("value", "{:f}".format(tdarr_api[0][i][j][k+2][l]["value"]))
-                        write_api.write(bucket=influx_bucket, org=influx_org, record=p)
+                    if tdarr_api[0][item_key] == "Stable":
+                        status = 0
                     else:
-                        p = influxdb_client.Point("tdarr").tag(tdarr_api[0]["_id"], libraries[k]).tag("library_name", tdarr_api[0][i][j][0]).tag("library_id", tdarr_api[0][i][j][1]).tag(field[k-4], no_space).field("value", "{:f}".format(tdarr_api[0][i][j][k+2][l]["value"]))
-                        write_api.write(bucket=influx_bucket, org=influx_org, record=p)
-                    l = l + 1
-                k = k + 1
-            j = j + 1
-    elif i == "streamStats":
-        for m in tdarr_api[0][i]:
-            for n in tdarr_api[0][i][m]:
-                p = influxdb_client.Point("tdarr").tag(tdarr_api[0]["_id"], i).tag("statistic", m).tag("range", n).field("value", "{:f}".format(tdarr_api[0][i][m][n]))
-                write_api.write(bucket=influx_bucket, org=influx_org, record=p)
-    elif i == "languages":
-        for o in tdarr_api[0][i]:
-            p = influxdb_client.Point("tdarr").tag(tdarr_api[0]["_id"], i).tag("language", o).field("value", "{:f}".format(tdarr_api[0][i][o]["count"]))
-            write_api.write(bucket=influx_bucket, org=influx_org, record=p)
+                        status = 9999
+                    influx_point = influxdb_client.Point("tdarr").tag(tdarr_api[0]["_id"], item_key).field("value", "{:f}".format(status))
+        elif item_key == "pies":
+            library_key = 0
+            while library_key < len(tdarr_api[0][item_key]):
+                library_stat = 0
+                while library_stat <= 3:
+                    influx_point = influxdb_client.Point("tdarr").tag(tdarr_api[0]["_id"], libraries[library_stat]).tag("library_name", tdarr_api[0][item_key][library_key][0]).tag("library_id", tdarr_api[0][item_key][library_key][1]).field("value", "{:f}".format(tdarr_api[0][item_key][library_key][library_stat+2]))
+                    library_stat = library_stat + 1
+                while library_stat < len(tdarr_api[0][item_key][library_key])-4:
+                    status_key = 0
+                    while status_key < len(tdarr_api[0][item_key][library_key][library_stat+2]):
+                        no_space = re.sub("\s","_",tdarr_api[0][item_key][library_key][library_stat+2][status_key]["name"])
+                        if library_stat < 1:
+                            influx_point = influxdb_client.Point("tdarr").tag(tdarr_api[0]["_id"], libraries[library_stat]).tag("library_name", tdarr_api[0][item_key][library_key][0]).tag("library_id", tdarr_api[0][item_key][library_key][1]).tag(field[library_stat-4], field_status[status_key]).field("value", "{:f}".format(tdarr_api[0][item_key][library_key][library_stat+2][status_key]["value"]))
+                        else:
+                            influx_point = influxdb_client.Point("tdarr").tag(tdarr_api[0]["_id"], libraries[library_stat]).tag("library_name", tdarr_api[0][item_key][library_key][0]).tag("library_id", tdarr_api[0][item_key][library_key][1]).tag(field[library_stat-4], no_space).field("value", "{:f}".format(tdarr_api[0][item_key][library_key][library_stat+2][status_key]["value"]))
+                        status_key = status_key + 1
+                    library_stat = library_stat + 1
+                library_key = library_key + 1
+        elif item_key == "streamStats":
+            for stream_stat in tdarr_api[0][item_key]:
+                for value_key in tdarr_api[0][item_key][stream_stat]:
+                    influx_point = influxdb_client.Point("tdarr").tag(tdarr_api[0]["_id"], item_key).tag("statistic", stream_stat).tag("range", value_key).field("value", "{:f}".format(tdarr_api[0][item_key][stream_stat][value_key]))
+        elif item_key == "languages":
+            for language_key in tdarr_api[0][item_key]:
+                influx_point = influxdb_client.Point("tdarr").tag(tdarr_api[0]["_id"], item_key).tag("language", language_key).field("value", "{:f}".format(tdarr_api[0][item_key][language_key]["count"]))
+        record.append(influx_point)
+
+    return record
+
+def write_influxdb(write_api, influx_bucket, influx_org, record):
+    """
+    Write the parsed Tdarr statistics to InfluxDB.
+
+    This function writes the parsed statistics data to the InfluxDBv2 bucket and organization from the config using the provided write API object.
+    It handles any exceptions that occur during the writing process.
+
+    Packages:
+        sys
+
+    Args:
+        write_api: The InfluxDB write API object.
+        influx_bucket (str): The name of the InfluxDB bucket.
+        influx_org (str): The name of the InfluxDB organization.
+        record (list): A list of InfluxDB data points representing the parsed statistics.
+
+    Returns:
+        None
+    """
+    try:
+        for influx_point in record:
+            write_api.write(bucket=influx_bucket, org=influx_org, record=influx_point)
+            print(influx_point)
+    except Exception as e:
+        print("An error occurred:", e)
+        sys.exit(1)
+
+def main():
+    """
+    Main entry point of the script.
+
+    This function is the main entry point of the script. It executes the previously defined functions to:
+        install missing packages, read the config, scrape the TDarr API, connect to InfluxDBv2, parse the Tdarr stats, and write it to InfluxDBv2.
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
+    install_missing_packages()
+    config = read_config()
+    tdarr_api = scrape_tdarr_api(config)
+    write_api, influx_bucket, influx_org = connect_influxdb(config)
+    record = parse_tdarr(tdarr_api)
+    write_influxdb(write_api, influx_bucket, influx_org, record)
+
+if __name__ == "__main__":
+    main()
